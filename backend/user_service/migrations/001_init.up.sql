6e2e87f8c7862423290abc5bdde5a5cbb62b4754
Nikita-Mihailuk <mihnik.9898@gmail.com>
2025-06-01 22:31:04 +0300

    added: added frontend in docker compose

--- backend/user_service/migrations/001_init.up.sql
+++ backend/user_service/migrations/001_init.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     role TEXT NOT NULL DEFAULT 'user'
 );
 
-INSERT INTO users (name, email, password_hash, photo_url) VALUES
-( 'test', 'test', '$2a$10$8ErcFt7uXtYabCzxFNmn5.Wk1Xo.jD1.dymb6cZq3MqVQH9aJv.OG', 'static/1.png'),
-( 'qwerty', 'qwerty', '$2a$10$73Mw4PWu9/eSfW4GcRqIu.kD0kQOyTk1va0YRQsdbTwSdB3VQdTa.', NULL),
-( 'nikita', 'nikita', '$2a$10$vStk0YDIgWlUZFaN5FFfVuUIcKSTlMmR/R4tR6gqONJufNhpClXyS', NULL);
+INSERT INTO users (name, email, password_hash) VALUES
+( 'Ильгиз', 'test@gmail.com', '$2a$10$8ErcFt7uXtYabCzxFNmn5.Wk1Xo.jD1.dymb6cZq3MqVQH9aJv.OG'),
+( 'Ильнур', 'qwerty@gmail.com', '$2a$10$73Mw4PWu9/eSfW4GcRqIu.kD0kQOyTk1va0YRQsdbTwSdB3VQdTa.'),
+( 'Никита', 'nikita@gmail.com', '$2a$10$vStk0YDIgWlUZFaN5FFfVuUIcKSTlMmR/R4tR6gqONJufNhpClXyS');
